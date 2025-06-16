ed5a839d573d3fdd4c3693f8a5399425aff94a41
Guilherme Almeida <guilhermehenriquegmil.com@gmail.com>
2025-06-16 18:51:16 -0300

    Add files via upload

---
+++ Exercícios/LPIA  Lógica da Programação com ChatGPT/Aula 01/Algoritimos_E_Váriavéis.py
@@ -0,0 +1,40 @@
+# Atividade 1.
+# Solicitar as Notas do Usuário.
+# Calcular a Média das Notas.
+# Mostrar a Média Calculada para o Usuário
+
+# bimestre_1 = float(input('Digite sua nota aqui: '))
+# bimestre_2 = float(input('Digite sua nota aqui: '))
+# bimestre_3 = float(input('Digite sua nota aqui: '))
+# bimestre_4 = float(input('Digite sua nota aqui: '))
+
+# soma = (bimestre_1 + bimestre_2 + bimestre_3 + bimestre_4) / 4
+
+# print(f'Essa é sua média final: {soma}')
+
+# Ativdade 2.
+# Solicitar o Salário Mensal.
+# Solicitar o Número de Horas Trabalhadas na Semana.
+# Calcular o Total de Horas Trabalhadas no Mês.
+# Calcular o Salário por Hora.
+# Mostrar o Salário por Hora Calculado para o Usuário.
+
+# salario_mes = float(input('Digite o valor do seu salário mensal: '))
+# horas_semana = int(input('Digite suas horas trabalhadas samanalmente: '))
+
+# horas_mes = (horas_semana) * 4
+# salario_hora = (salario_mes) / 30
+
+# print(f'Você trabalha {horas_mes} horas por mês e ganha {salario_hora} reais por hora. ')
+
+# Atividade 3.
+# Solicitar o Nome do Usuário.
+# Solicitar a Idade do Usuário.
+# Solicitar a Cidade Natal do Usuário.
+# Formatar e Exibir a Mensagem com f-string.
+
+# nome = str(input('Digite seu nome aqui: '))
+# idade = int(input('Digite sua idade aqui: '))
+# cidade = str(input('Digite sua cidade aqui: '))
+
+# print(f'Seu nome é {nome}, você tem {idade} anos e mora em {cidade}.')
